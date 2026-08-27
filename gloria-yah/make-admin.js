d8f8d52717b463f1d8eff1ddc5c273c75a0975ac
Ton Nom <tonemail@gmail.com>
2026-08-27 14:19:01 +0100

    Formulaire pilote combine, semaine de lancement, fidelite passager, paiement passager

---
+++ gloria-yah/make-admin.js
@@ -0,0 +1,6 @@
+﻿require('dotenv').config();
+const pool = require('./db/pool');
+const phone = process.argv[2];
+pool.query('UPDATE users SET role = $1 WHERE phone_number = $2', ['ADMIN', phone])
+  .then(r => { console.log('Lignes modifiées :', r.rowCount); process.exit(); })
+  .catch(err => { console.error('Erreur :', err.message); process.exit(1); });
